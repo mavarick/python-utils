5a95af3a7603e1ffe8e6d019a5524b92f59650ab
Mavarick <mavarick@XufengdeMBP.lan>
2016-04-12 18:17:14 +0800

    add: baidu translate api

---
+++ tools/translate.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#encoding:utf8
+
+import requests
+import json
+import urllib
+import copy
+
+
+RETURN_DATA = {
+    "code": 0,
+    "msg": "",
+    "data": ""
+}
+
+class TranslateAPI(object):
+    BAIDU_API = "http://fanyi.baidu.com/v2transapi/?from={0}&to={1}&query={2}"
+
+    @staticmethod
+    def translate(query, f="en", t="zh"):
+        '''
+        :param query: text query, must be utf8
+        :param f:  from, default is 'en'
+        :param t:  to, default is 'zh'
+        :return:   return text
+        '''
+        query = query.strip()
+        if not query:
+            raise Exception("No query data")
+        query = urllib.quote(query)
+        url = TranslateAPI.BAIDU_API.format(f, t, query)
+
+        resp = requests.get(url)
+        data = json.loads(resp.content)
+        if "error" in data:
+            code = data.get("code", -1)
+            msg = data.get("msg", "")
+            raise Exception("request error: code: [%s], msg: [%s]"%(code, msg))
+        tran_text = data['trans_result']['data'][0]['dst']
+        return tran_text
+
+def test():
+    text = "this is a new world"
+    print "这是一个新的世界"
+    tran = TranslateAPI.translate(text)
+    print text
+    print ">> [%s]"%tran
+
+if __name__ == "__main__":
+    test()
+
+
+
+
+
+
